168ecef72bc60a4a9b19294d882a538e2336931d
Albert Sanchez <singletons@goat.si>
2021-10-23 11:11:27 -0500

    black pastas

--- pseudbot/pastas.py
+++ pseudbot/pastas.py
@@ -1,3 +1,6 @@
+# NOTE: Each pasta tweet should be no more than 263 characters long!
+
+
 def get_pastas() -> list:
     pfile = open("pastas.txt", mode="r")
     pastas = pfile.read().split("\n\n")
